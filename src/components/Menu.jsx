dda6826fb4926aa2e6b2fd0f2b7c60c933116ed9
BRUNO SAIKI <saiki.bruno@gmail.com>
2022-03-22 22:26:20 -0300

    primeiro commit

---
+++ src/components/Menu.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Menu = () => {
+  return (
+    <div>
+      Menu
+      <ul>
+        <li>
+          <a href=""></a>
+          Pagina 1
+        </li>
+        <li>
+          <a href=""></a>
+          Pagina 2
+        </li>
+        <li>
+          <a href=""></a>
+          Pagina 3
+        </li>
+      </ul>
+
+      
+    </div>
+  );
+};
+
+export default Menu;
